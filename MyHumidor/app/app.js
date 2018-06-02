546fec1cc0305042e6514f7aac7957def797eb67
developer <matt.hensley777@gmail.com>
2018-06-02 11:31:31 -0500

    html showing

--- MyHumidor/app/app.js
+++ MyHumidor/app/app.js
@@ -1,1 +1,19 @@
 ﻿var app = angular.module("MyHumidor", ['ngRoute']);
+
+app.config([
+    '$routeProvider', function ($routeProvider) {
+        $routeProvider
+            
+            .when("/cigars",
+            {
+                templateUrl: '/app/partials/Cigars.html',
+                controller: 'CigarController'
+            })
+            .when("/cigars/new",
+            {
+                templateUrl: '/app/partials/new_cigar.html',
+                controller: 'NewCigarController'
+            })
+            
+    }
+]);
