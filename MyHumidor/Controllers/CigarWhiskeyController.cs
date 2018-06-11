4b769709830d4250bbcbdaa49a4dff18fd2e3398
developer <matt.hensley777@gmail.com>
2018-06-11 15:51:42 -0500

    starting join tables

---
+++ MyHumidor/Controllers/CigarWhiskeyController.cs
@@ -0,0 +1,34 @@
+﻿using MyHumidor.Services;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyHumidor.Controllers
+{
+    [RoutePrefix("api/cigars")]
+    public class EmployeeComputerController : ApiController
+    {
+        [Route("{id}"), HttpGet]
+        public HttpResponseMessage GetByCigarId(int id)
+        {
+            var repo = new CigarWhiskeyRepository();
+            var result = repo.GetByCigarId(id);
+
+
+            return (result != null)
+                ? Request.CreateResponse(HttpStatusCode.OK, result)
+                : Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Could not get Cigar by ID");
+        }
+
+        [Route("{id}/cigar"), HttpGet]
+        public HttpResponseMessage GetByWhiskeyId(int id)
+        {
+            var repo = new CigarWhiskeyRepository();
+            var result = repo.GetByWhiskeyId(id);
+
+            return (result != null)
+                ? Request.CreateResponse(HttpStatusCode.OK, result)
+                : Request.CreateResponse(HttpStatusCode.NoContent, result = null);
+        }
+    }
+}
---
+++ MyHumidor/Services/CigarWhiskeyRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using MyHumidor.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MyHumidor.Services
+{
+    public class CigarWhiskeyRepository
+    {
+        private static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["MyHumidor"].ConnectionString);
+        }
+
+        public WhiskeyCigarDTO GetByCigarId(int id)
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+                var result = db.QueryFirstOrDefault<WhiskeyCigarDTO>(@"select * from WhiskeyCigar where CigarID = @id", new { id });
+
+                return result;
+            }
+        }
+        public WhiskeyCigarDTO GetByWhiskeyId(int id)
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+                var result = db.QueryFirstOrDefault<WhiskeyCigarDTO>(@"select * from WhiskeyCigar where WhiskeyID = @id", new { id });
+
+                return result;
+            }
+        }
+    }
+}
