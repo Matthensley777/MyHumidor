68c26fb71e6e0c82cc96d5eec5bb5753fb732abc
developer <matt.hensley777@gmail.com>
2018-06-13 20:55:19 -0500

    starting user

---
+++ MyHumidor/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using MyHumidor.Services;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyHumidor.Controllers
+{
+    [RoutePrefix("api/User")]
+    public class UserController : ApiController
+    {
+        [Route("{id}/UserID"), HttpGet]
+        public HttpResponseMessage GetUserById(int id)
+        {
+            var repo = new UserRepository();
+            var result = repo.GetUserById(id);
+
+
+            return (result != null)
+                ? Request.CreateResponse(HttpStatusCode.OK, result)
+                : Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Not a User");
+        }
+
+        [Route("{id}/Cigar"), HttpGet]
+        public HttpResponseMessage GetUserByCigarId(int id)
+        {
+            var repo = new CigarRepository();
+            var result = repo.GetUserByCigarId(id);
+
+            return (result != null)
+                ? Request.CreateResponse(HttpStatusCode.OK, result)
+                : Request.CreateResponse(HttpStatusCode.InternalServerError, "no Cigar for you!");
+        }
+    }
+}
---
+++ MyHumidor/Services/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using MyHumidor.Models;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MyHumidor.Services
+{
+    public class UserRepository
+    {
+        private static SqlConnection GetConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["MyHumidor"].ConnectionString);
+        }
+
+        public UserDTO GetUserByCigarId(int id)
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+                var result = db.QueryFirstOrDefault<UserDTO>(@"select * from User where UserID = @id", new { id });
+
+                return result;
+            }
+        }
+        public CigarDTO GetByCigarId(int id)
+        {
+            using (var db = GetConnection())
+            {
+                db.Open();
+                var result = db.QueryFirstOrDefault<CigarDTO>(@"select * from Cigar where CigarID = @id", new { id });
+
+                return result;
+            }
+        }
+    }
+}
