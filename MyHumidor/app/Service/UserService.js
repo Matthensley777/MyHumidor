95257f4d17d5dd73d5a0c494237b2f7ea04f30be
developer <matt.hensley777@gmail.com>
2018-06-19 18:34:18 -0500

    pretty much done

--- MyHumidor/app/Service/UserService.js
+++ MyHumidor/app/Service/UserService.js
@@ -8,21 +8,5 @@
     }
 
 
-    //const getUserCigars = (userId) => {
-    //    let userCigars = [];
-    //    return $q((resolve, reject) => {
-    //        $http.get(`${databaseURL}/"${userId}"`).then((results) => {
-    //            let cigars = results.data;
-    //            Object.keys(photos).forEach((key) => {
-    //                cigars[key].id = key;
-    //                userCigars.push(cigars[key]);
-    //            });
-    //            resolve(userCigars);
-    //        }).catch((error) => {
-    //            reject(error);
-    //        });
-    //    });
-    //};
-
 
 });
